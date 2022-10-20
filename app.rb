3d34d72564cf416c5b14a256780774f5bc158731
charity <wa.nelimacharity@gmail.com>
2022-10-20 12:50:22 +0300

    learn ruby print,puts

--- app.rb
+++ app.rb
@@ -1,2 +1,7 @@
-pp [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
+#pp [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
 
+puts "Hello World!"
+
+print "Pass this test, please."
+
+p [1, 2, 3]
